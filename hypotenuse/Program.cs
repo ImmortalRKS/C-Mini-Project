c2719ae65acc1e298ee5636b7664a55b01d9e5ba
Immortal <rishitks1307@gmail.com>
2026-06-20 11:37:31 +0530

    1st ver.

---
+++ hypotenuse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("This Program will allow you to find hypotenuse of a traingle");
+Console.WriteLine("Enter both sides of the traingle");
+
+Console.Write("Enter side A : ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter side B : ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double c = Math.Sqrt((a*a) + (b*b));
+Console.WriteLine("The hypotenuse of the given side is : " + c);
+
+// This program help us to find hypotenuse of the traingle
